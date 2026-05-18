d30a07782d126bc5c4da8164919bf662e76933b5
Tiago Rocha <TGROCHA605@GMAIL.COM>
2026-05-18 19:04:20 +0100

    cria canl de notificacoes na MindStepApp

--- app/src/main/java/com/ctrlzgod/mindstep/MindStepApplication.kt
+++ app/src/main/java/com/ctrlzgod/mindstep/MindStepApplication.kt
@@ -1,16 +1,46 @@
 package com.ctrlzgod.mindstep
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import androidx.room.Room
 import com.ctrlzgod.mindstep.data.local.MindStepDatabase
 
 class MindStepApplication : Application() {
 
+    // O teu cofre de dados (Intacto!)
     val database: MindStepDatabase by lazy {
         Room.databaseBuilder(
             this,
             MindStepDatabase::class.java,
-            "mindstep_database" // nome guardado no telefoen
+            "mindstep_database" // nome guardado no telefone
         ).build()
+    }
+
+    // Esta função corre automaticamente assim que a app é aberta
+    override fun onCreate() {
+        super.onCreate()
+        createNotificationChannel()
+    }
+
+    private fun createNotificationChannel() {
+        // O Android só exige Canais a partir da versão 8.0 (Oreo - API 26)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "Lembretes MindStep"
+            val descriptionText = "Avisos importantes para beber água e meditar"
+
+            // IMPORTANCE_HIGH garante que o telemóvel faz som e mostra um pop-up no topo do ecrã
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val channel = NotificationChannel("mindstep_reminders", name, importance).apply {
+                description = descriptionText
+            }
+
+            // Registar o canal no Sistema Android
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 }
